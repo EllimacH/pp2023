7080a929d9141e08ced4411b5c8ea736bfc6e898
EllimacH <QuangHM.BI12-370@st.usth.edu.vn>
2023-02-19 17:50:44 +0700

    Refactor labwork 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,49 +1,107 @@
-# function to input student information
-student_list = []
-course_list = []
+import os
+import re
 
+def input_student(students: list) -> None:
+    temp = {}
+    student_name = input("Enter student name: ")
+    student_id = input("Enter student id: ").upper()
 
-def input_student():
-    global number
-    number = int(input("Enter the number of students: "))
-    for i in range(number):
-        id = str(input("Enter the id of the student: "))
-        name = input("Enter the name of the student: ")
-        DoB = str(input("Enter the date of birth of the student: "))
-        student = {"Name: ": name, "ID: ": id, "DoB: ": DoB}
-        student_list.append(student)
-    return student_list
-
-
-def input_course():
-    numberCourse = int(input("Enter the number of courses: "))
-    for i in range(numberCourse):
-        nameCourse = input("Enter the name of the course: ")
-        course = {"Name: ": nameCourse}
-        corse_id = str(input("Enter the id of the course: "))
-        course_list.append(course)
-    return course_list
-
-
-def input_mark(student_list: list, course_list: list):
-    Mark = float(input("Enter the mark of the student: "))
     while True:
-        try:
-            if Mark < 0 or Mark > 20:
-                raise ValueError
+        # if student id is BIxx-xxx (x is digit)
+        if re.match(r"BI\d{2}-\d{3}", student_id):
             break
-        except ValueError:
-            Mark = float(input("Invalid mark, please try again: "))
+        else:
+            student_id = input("Student id is not valid, please try again: ")
+        
+    DoB = input("Enter student DoB: ")
+    # if DoB is xx-xx-xxxx or xx/xx/xxxx
+    while True:
+        if re.match(r"\d{2}-\d{2}-\d{4}", DoB) or re.match(r"\d{2}/\d{2}/\d{4}", DoB):
+            break
+        else:
+            DoB = input("DoB is not valid, please try again: ")
 
-    for course in course_list:
-        print(course)
-    select_course = input("Select the course: ")
+    temp["student_name"] = student_name
+    temp["student_id"] = student_id
+    temp["Date of birth"] = DoB
+    temp["course"] = []
+    students.append(temp)
 
-    for i in range(number):
-        for i in range(number):
-            student_list[i].append(int(input("Grade for student " + str(i+1))))
+def input_course(courses: list) -> None:
+    temp = {}
+    course_name = input('Enter course name: ')
+    course_id = input('Enter course id: ')
+    temp["course_name"] = course_name
+    temp["course_id"] = course_id
+    courses.append(temp)
+
+def input_mark(students: list, courses: list) -> None:
+    student_id_input = input("Enter student id you want to input mark: ")
+    course_id_input = input("Enter course id to input mark: ")
+    mark = input("Enter mark: ")
+    course_name_searced = ""
+
+    for course in courses:
+        if course["course_id"] == course_id_input:
+            course_name = course["course_name"] 
+            break
+        
+    
+    for student in students:
+        if student["student_id"] == student_id_input:
+            student["course"].append({"course_name": course_name, "mark": mark})
+            break
+    
+def main ():
+    students = []
+    courses = []
 
 
-input_student()
-input_course()
-input_mark(student_list, course_list)
+    while True:
+
+        print("\033c")
+
+        print("1. Input student")
+        print("2. Input course")
+        print("3. Input mark")
+        print("4. Print student")
+        print("5. Print courses")
+        print("6. Exit")
+        
+        choice = input("Enter your option: ")
+        while True:
+            try:
+                choice = int(choice)
+                if choice in range (1,7):
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                choice = input("Input not valid , please try again: ")
+
+        match choice:
+            case 1: 
+                input_student(students)
+            case 2:
+                input_course(courses)
+            case 3:
+                input_mark(students,courses)
+            case 4:
+                for student in students:
+                    print("="*20)
+                    print("Student name: " + student["student_name"])
+                    print("Student id: " + student["student_id"])
+                    print("Date of birth: " + student["Date of birth"])
+                    for course in student["course"]:
+                        # print("Course: " + course["course_name"] + ": " + course["mark"])
+                        print(f"Course: {course['course_name']}: {course['mark']}")
+            case 5:
+                for course in courses:
+                    print(course)
+            case 6:
+                break
+
+        input("Enter to continue...")
+
+if __name__ == "__main__":
+    main()
